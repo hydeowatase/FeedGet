e94bae14aa3d2f1b6a60122674cc2e6e01abf9cb
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-05-09 20:03:51 -0300

    Insta Posting

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,8 +1,62 @@
+import { CloseButton } from "./CloseButton";
+
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
 export function WidgetForm() {
   // w-[calc(100vm-2rem) =  100% do view-port-width menos 2 rem
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button>
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://www.linkedin.com/in/hydeo-mesquita-watase-1a9924127/"
+        >
+          HMW - Solution
+        </a>
+      </footer>
     </div>
   );
 }

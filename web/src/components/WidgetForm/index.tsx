ab70a59ca704f28804e30c5d066d11f52d6e3f76
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-05-09 23:33:59 -0300

    feedback content

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
 
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -38,12 +38,11 @@ export function WidgetForm() {
   // w-[calc(100vm-2rem) =  100% do view-port-width menos 2 rem
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
-      {!feedbackType ? <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} /> : <p>Hello World</p>}
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep feedbackType={feedbackType} />
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}

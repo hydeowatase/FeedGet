3a29ce51b68f1c0bdc20f63ce76d0d49c5fb3dd7
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-05-17 12:34:37 -0300

    Feedget taildwindcss

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,13 +1,15 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
+  onFeeedbackRestartRequested: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
+  onFeeedbackRestartRequested,
 }: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
@@ -16,7 +18,8 @@ export function FeedbackContentStep({
       <header>
         <button
           type="button"
-          className="top-5 left-5 text-zinc-400 hover:text-zinc-100"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeeedbackRestartRequested}
         >
           <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
@@ -30,7 +33,20 @@ export function FeedbackContentStep({
         </span>
         <CloseButton />
       </header>
-      <div className="flex py-8 gap-2 w-full"></div>
+      <form className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-thin"
+          placeholder="Tell in detail what's happening"
+        />
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="submit"
+            className="p-2 bg-brand-500  rounded-md  border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -35,13 +35,20 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  const handleRestartFeedback = () => {
+    setFeedbackType(null);
+  };
+
   // w-[calc(100vm-2rem) =  100% do view-port-width menos 2 rem
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType} />
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
